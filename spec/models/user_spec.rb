03ca1dcac679286b49b8670e0e9328557f20ef3b
deluxe-datex <68992615+deluxe-datex@users.noreply.github.com>
2020-08-24 14:17:58 +0900

    バリデーションの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー情報' do
+    it "nicknameが必須であること" do
+      
+    end
+    it "メールアドレスが必須であること" do
+    end
+    it "メールアドレスが一意性であること" do
+    end
+    it "メールアドレスは＠を含む必要があること" do
+    end
+    it "パスワードが必須であること" do
+    end
+    it "パスワードは６文字以上であること" do
+    end
+    it "パスワードは半角英数字混合であること" do
+    end
+    it "パスワードは確認用を含めて２回入力すること" do
+    end
+      
+    end
+  end
 end
