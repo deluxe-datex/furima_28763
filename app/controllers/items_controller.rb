c68e9c82f3962569fdbe1bab850e4471d76fc08e
deluxe-datex <68992615+deluxe-datex@users.noreply.github.com>
2020-08-26 15:24:47 +0900

    ログインユーザーだけが出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,20 @@
 class ItemsController < ApplicationController
+  before_action :login_check, only: :new
+
   def index
   end
   
   def new
+
   end
+
+  private
+
+  def login_check
+    unless user_signed_in?
+      
+      redirect_to root_path
+    end
+  end
+
 end
