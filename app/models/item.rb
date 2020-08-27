5e1eac3daaf7e19b220e7df2bab9f11436a76f0e
deluxe-datex <68992615+deluxe-datex@users.noreply.github.com>
2020-08-27 11:53:48 +0900

    statusモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
+  belongs_to_active_hash :category, :status
 
   belongs_to :user
   has_one :trading_history
   has_one_attached :image
 
   validates :name, presence: true
-  validates :category_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }  
 end
--- db/migrate/20200826020155_create_items.rb
+++ db/migrate/20200826020155_create_items.rb
@@ -13,6 +13,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :price,      null: false
       t.references :user,    null: false, foreign_key: true
       t.integer :category_id, null: false
+      t.integer :status_id, null: false
       t.timestamps
     end
   end
