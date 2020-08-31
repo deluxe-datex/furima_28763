e4ac3054b59a5b95d1577e9151beafa3480f5194
deluxe-datex <68992615+deluxe-datex@users.noreply.github.com>
2020-08-31 16:59:41 +0900

    出品順を並び替え

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :login_check, only: :new
 
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
   
   def new
