04b04d2191c86d8d6773be0bed4130c7d784a3c7
deluxe-datex <68992615+deluxe-datex@users.noreply.github.com>
2020-09-01 11:01:41 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :users, only: [:edit, :update]
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show, :edit, :destroy]
   get 'item/:id', to: 'item#tax'
 end
