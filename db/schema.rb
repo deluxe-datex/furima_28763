bbb5d0500bef5a69441318526b59b473b4ded8c9
deluxe-datex <68992615+deluxe-datex@users.noreply.github.com>
2020-09-17 18:42:43 +0900

    SnsCredentialモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_081343) do
+ActiveRecord::Schema.define(version: 2020_09_17_062136) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2020_09_04_081343) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "trading_histories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
@@ -91,6 +100,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_081343) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "destinations", "trading_histories"
   add_foreign_key "items", "users"
+  add_foreign_key "sns_credentials", "users"
   add_foreign_key "trading_histories", "items"
   add_foreign_key "trading_histories", "users"
 end
